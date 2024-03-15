ace9b15765d9b0bd755971a6cf3c3fbdded5be96
pires_juliano <163434895+JulianoPPires@users.noreply.github.com>
2024-03-15 06:02:52 -0300

    frontend funcional sem estilizacao

--- frontend/splitpay-app/src/components/HelloWorld.vue
+++ frontend/splitpay-app/src/components/HelloWorld.vue
@@ -14,6 +14,35 @@
         <input type="number" v-model="item.value" :placeholder="'Valor ' + (itemIndex + 1)" />
       </div>
       <button @click="addParticipantItem(participantIndex)">Adicionar Item</button>
+      <button v-if="participantIndex > 0" @click="removeParticipant(participantIndex)">Remover Participante</button>
+    </div>
+
+    <!-- Campo para gastos adicionais -->
+    <div>
+      <h3>Gastos Adicionais</h3>
+      <div v-for="(expense, index) in additionalExpenses" :key="index">
+        <input type="text" v-model="expense.name" :placeholder="'Nome do gasto ' + (index + 1)" />
+        <input type="text" v-model="expense.value" :placeholder="'Valor ' + (index + 1)" />
+        <select v-model="expense.isPercentage">
+          <option value="true">Porcentagem</option>
+          <option value="false">Valor Fixo</option>
+        </select>
+      </div>
+      <button @click="addExpense">Adicionar Gasto</button>
+    </div>
+
+    <!-- Campo para descontos adicionais -->
+    <div>
+      <h3>Descontos Adicionais</h3>
+      <div v-for="(discount, index) in additionalDiscounts" :key="index">
+        <input type="text" v-model="discount.name" :placeholder="'Nome do desconto ' + (index + 1)" />
+        <input type="text" v-model="discount.value" :placeholder="'Valor ' + (index + 1)" />
+        <select v-model="discount.isPercentage">
+          <option value="true">Porcentagem</option>
+          <option value="false">Valor Fixo</option>
+        </select>
+      </div>
+      <button @click="addDiscount">Adicionar Desconto</button>
     </div>
 
     <!-- Botão para adicionar participante -->
@@ -28,7 +57,9 @@
 export default {
   data() {
     return {
-      participants: [{ name: '', items: [{ name: '', value: '' }] }] 
+      participants: [{ name: '', items: [{ name: '', value: '' }] }],
+      additionalExpenses: [{ name: '', value: '', isPercentage: false }],
+      additionalDiscounts: [{ name: '', value: '', isPercentage: false }]
     };
   },
   methods: {
@@ -38,8 +69,19 @@ export default {
     addParticipantItem(participantIndex) {
       this.participants[participantIndex].items.push({ name: '', value: '' });
     },
+    removeParticipant(participantIndex) {
+      this.participants.splice(participantIndex, 1);
+    },
+    addExpense() {
+      this.additionalExpenses.push({ name: '', value: '', isPercentage: false });
+    },
+    addDiscount() {
+      this.additionalDiscounts.push({ name: '', value: '', isPercentage: false });
+    },
     saveAndDivide() {
       console.log('Dados dos participantes:', this.participants);
+      console.log('Gastos Adicionais:', this.additionalExpenses);
+      console.log('Descontos Adicionais:', this.additionalDiscounts);
     }
   }
 };
