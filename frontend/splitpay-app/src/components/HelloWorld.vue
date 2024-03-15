d55c841694deb0909b44976d82384622d8865653
pires_juliano <163434895+JulianoPPires@users.noreply.github.com>
2024-03-15 05:49:09 -0300

    correcao front

--- frontend/splitpay-app/src/components/HelloWorld.vue
+++ frontend/splitpay-app/src/components/HelloWorld.vue
@@ -2,30 +2,22 @@
   <div>
     <h2>Formulário de Compras</h2>
 
-    <!-- Formulário para a pessoa -->
-    <div>
-      <h3>Seus Itens</h3>
-      <div v-for="(item, index) in items" :key="index">
-        <input type="text" v-model="item.name" placeholder="Nome do item" />
-        <input type="number" v-model="item.value" placeholder="Valor" />
+    <!-- Formulário para cada participante -->
+    <div v-for="(participant, participantIndex) in participants" :key="participantIndex">
+      <h3>Participante {{ participantIndex + 1 }}</h3>
+      <div>
+        <label>Nome:</label>
+        <input type="text" v-model="participant.name" />
       </div>
-      <button @click="addItem">Adicionar Item</button>
+      <div v-for="(item, itemIndex) in participant.items" :key="itemIndex">
+        <input type="text" v-model="item.name" :placeholder="'Nome do item ' + (itemIndex + 1)" />
+        <input type="number" v-model="item.value" :placeholder="'Valor ' + (itemIndex + 1)" />
+      </div>
+      <button @click="addParticipantItem(participantIndex)">Adicionar Item</button>
     </div>
 
-    <!-- Formulário para amigos -->
-    <div>
-      <h3>Amigos</h3>
-      <div v-for="(friend, friendIndex) in friends" :key="friendIndex">
-        <h4>Amigo {{ friendIndex + 1 }}</h4>
-        <input type="text" v-model="friend.name" placeholder="Nome do amigo" />
-        <div v-for="(item, itemIndex) in friend.items" :key="itemIndex">
-          <input type="text" v-model="item.name" :placeholder="'Nome do item ' + (itemIndex + 1)" />
-          <input type="number" v-model="item.value" :placeholder="'Valor ' + (itemIndex + 1)" />
-        </div>
-        <button @click="addFriendItem(friendIndex)">Adicionar Item</button>
-      </div>
-      <button @click="addFriend">Adicionar Amigo</button>
-    </div>
+    <!-- Botão para adicionar participante -->
+    <button @click="addParticipant">Adicionar Participante</button>
 
     <!-- Botão para salvar e dividir -->
     <button @click="saveAndDivide">Salvar e Dividir</button>
@@ -36,30 +28,22 @@
 export default {
   data() {
     return {
-      items: [{ name: '', value: '' }], // Itens da pessoa
-      friends: [{ name: '', items: [{ name: '', value: '' }] }] // Amigos e seus itens
+      participants: [{ name: '', items: [{ name: '', value: '' }] }] 
     };
   },
   methods: {
-    addItem() {
-      this.items.push({ name: '', value: '' });
+    addParticipant() {
+      this.participants.push({ name: '', items: [{ name: '', value: '' }] });
     },
-    addFriendItem(friendIndex) {
-      this.friends[friendIndex].items.push({ name: '', value: '' });
-    },
-    addFriend() {
-      this.friends.push({ name: '', items: [{ name: '', value: '' }] });
+    addParticipantItem(participantIndex) {
+      this.participants[participantIndex].items.push({ name: '', value: '' });
     },
     saveAndDivide() {
-      // Aqui você pode salvar os dados ou realizar a lógica de divisão dos valores
-      console.log('Itens da pessoa:', this.items);
-      console.log('Amigos:', this.friends);
-      // Lógica de divisão dos valores...
+      console.log('Dados dos participantes:', this.participants);
     }
   }
 };
 </script>
 
 <style>
-/* Adicione estilos conforme necessário */
 </style>
