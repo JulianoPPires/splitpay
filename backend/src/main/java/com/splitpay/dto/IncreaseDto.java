76e47065a10bc81b1058f00d53e8516389c71fb7
pires_juliano <163434895+JulianoPPires@users.noreply.github.com>
2024-03-19 11:39:37 -0300

    correcao swagger

--- backend/src/main/java/com/splitpay/dto/IncreaseDto.java
+++ backend/src/main/java/com/splitpay/dto/IncreaseDto.java
@@ -1,5 +1,6 @@
 package com.splitpay.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,6 +17,7 @@ public class IncreaseDto {
   private Double value;
 
   @NotNull
+  @Schema(example = "false")
   private Boolean isPercentage;
 
 }
